7b9f88c2291b020376a25196a515eda324e4bb0b
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-03-04 13:17:24 +0100

    change col structural reduction timeout to 30 from 60 seconds

--- include/PetriEngine/options.h
+++ include/PetriEngine/options.h
@@ -66,7 +66,7 @@ struct options_t {
     std::vector<uint32_t> reductions{8,2,3,4,5,7,9,6,0,1};
     std::vector<uint32_t> colreductions{};
     int reductionTimeout = 60;
-    int colReductionTimeout = 60;
+    int colReductionTimeout = 30;
     bool stubbornreduction = true;
     bool statespaceexploration = false;
     bool printstatistics = true;
