12cb7d45abda225ea3300239b9972991ed486c0b
NicEastvillage <nicoesterby@gmail.com>
2022-03-17 09:48:21 +0100

    Fix errors introduced by merge

--- include/PetriEngine/Colored/Reduction/RedRuleParallelTransitions.h
+++ include/PetriEngine/Colored/Reduction/RedRuleParallelTransitions.h
@@ -17,6 +17,10 @@ namespace PetriEngine::Colored::Reduction {
 
         bool canBeAppliedRepeatedly() override { return false; }
 
+        bool isApplicable(QueryType queryType, bool preserveLoops, bool preserveStutter) const override {
+            return !preserveStutter;
+        }
+
         bool apply(ColoredReducer &red, const std::vector<bool> &inQuery, QueryType queryType, bool preserveLoops, bool preserveStutter) override;
     };
 }
