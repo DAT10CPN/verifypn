c5f7d81a256f0a36582678fad82b4e1100692aab
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-03-18 10:28:28 +0100

    swap order of prototypes

--- include/PetriEngine/Colored/PnmlWriter.h
+++ include/PetriEngine/Colored/PnmlWriter.h
@@ -4,20 +4,48 @@
 
 #ifndef VERIFYPN_PNMLWRITER_H
 #define VERIFYPN_PNMLWRITER_H
+
 #include "utils/errors.h"
 #include "ColoredPetriNetBuilder.h"
 
 namespace PetriEngine::Colored {
     class PnmlWriter {
     public:
-        PnmlWriter(PetriEngine::ColoredPetriNetBuilder &b, std::ostream& out) : _builder(b), _out(out), _tabsCount(0) {}
+        PnmlWriter(PetriEngine::ColoredPetriNetBuilder &b, std::ostream &out) : _builder(b), _out(out), _tabsCount(0) {}
+
         void toColPNML();
 
     private:
         PetriEngine::ColoredPetriNetBuilder &_builder;
-        std::ostream& _out;
+        std::ostream &_out;
         std::uint32_t _tabsCount;
-        std::vector <Arc> _arcs;
+        std::vector<Arc> _arcs;
+
+        uint32_t getTabsCount() {
+            return _tabsCount;
+        }
+
+        std::string getTabs() {
+            std::string tabsString;
+            for (uint32_t i = 0; i < _tabsCount; i++) {
+                tabsString += '\t';
+            }
+            return tabsString;
+        }
+
+        std::string increaseTabs() {
+            _tabsCount += 1;
+            return getTabs();
+        }
+
+        std::string decreaseTabs() {
+            if (_tabsCount == 0) {
+                throw base_error(
+                        "Something went wrong with exporting colored model to PNML - underflow in number of tabs");
+            }
+            _tabsCount -= 1;
+            return getTabs();
+        }
 
         void metaInfo();
 
@@ -33,36 +61,11 @@ namespace PetriEngine::Colored {
 
         void page();
 
-        uint32_t getTabsCount() {
-            return _tabsCount;
-        }
-
-        std::string getTabs() {
-            std::string tabsString;
-            for (uint32_t i=0; i < _tabsCount; i++) {
-                tabsString += '\t';
-            }
-            return tabsString;
-        }
-
-        std::string increaseTabs() {
-            _tabsCount += 1;
-            return getTabs();
-        }
-
-        std::string decreaseTabs() {
-            if (_tabsCount == 0) {
-                throw base_error("Something went wrong with exporting colored model to PNML - underflow in number of tabs");
-            }
-            _tabsCount -= 1;
-            return getTabs();
-        }
-
         void handleProductSort(std::vector<const ColorType *> types);
 
         void handleCyclicEnumeration(std::vector<const ColorType *> types);
 
-        void handleFiniteRange(const std::vector<const ColorType *>& types);
+        void handleFiniteRange(const std::vector<const ColorType *> &types);
 
         void handleVariables();
 
@@ -74,7 +77,7 @@ namespace PetriEngine::Colored {
 
         void add_arcs_from_transition(Transition &transition);
 
-        void handleCondition(Colored::Transition& transition);
+        void handleCondition(Colored::Transition &transition);
 
         void handleNow(std::vector<std::string> productSorts, std::vector<std::string> cyclicEnumerations);
 
