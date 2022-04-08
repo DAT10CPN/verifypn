4df6c22f96ccb00094f0fc78f0ac0c1120eb3685
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-04-08 10:36:40 +0200

    took code from earlier rule f, and adapted to col

--- src/PetriEngine/Colored/Reduction/RedRuleRedundantPlaces.cpp
+++ src/PetriEngine/Colored/Reduction/RedRuleRedundantPlaces.cpp
@@ -11,8 +11,45 @@
 
 namespace PetriEngine::Colored::Reduction {
     bool RedRuleRedundantPlaces::apply(ColoredReducer &red, const std::vector<bool> &inQuery,
-                                      QueryType queryType, bool preserveLoops, bool preserveStutter) {
-        bool continueReductions = true;
+                                       QueryType queryType, bool preserveLoops, bool preserveStutter) {
+        bool continueReductions = false;
+        const size_t numberofplaces = red.placeCount();
+        for (uint32_t p = 0; p < numberofplaces; ++p) {
+            if (red.hasTimedOut()) return false;
+            Place place = red.places()[p];
+            if (place.skipped) continue;
+            if (place.inhibitor) continue;
+            if (place._pre.size() < place._post.size()) continue;
+            if (inQuery[p] != 0) continue;
+
+            bool ok = true;
+            for (uint cons: place._pre) {
+                Transition t = red.transitions()[cons];
+                auto w = red.getInArc(p, t)->expr->weight();
+                if (w > place.marking.size()) {
+                    ok = false;
+                    break;
+                } else {
+                    auto it = red.getOutArc(t, p);
+                    if (it == t.output_arcs.end() ||
+                        it->place != p ||
+                        it->expr->weight() < w) {
+                        ok = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!ok) continue;
+
+            ++_applications;
+
+            if (red.unskippedPlacesCount() > 1) {
+                red.skipPlace(p);
+                continueReductions = true;
+            }
+        }
+        red.consistent();
         return continueReductions;
     }
 }
