1985867617df7a80d8e084c0cba6a5b049328667
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-05-05 11:24:11 +0200

    move comments

--- src/PetriEngine/Colored/Reduction/RedRulePreemptiveFiring.cpp
+++ src/PetriEngine/Colored/Reduction/RedRulePreemptiveFiring.cpp
@@ -4,7 +4,9 @@
  *      Jesper Adriaan van Diepen
  *      Mathias Mehl Sørensen
  */
-
+#include <PetriEngine/Colored/PartitionBuilder.h>
+#include <PetriEngine/Colored/EvaluationVisitor.h>
+#include <PetriEngine/Colored/BindingGenerator.h>
 #include "PetriEngine/Colored/Reduction/RedRulePreemptiveFiring.h"
 #include "PetriEngine/Colored/Reduction/ColoredReducer.h"
 #include "PetriEngine/Colored/ArcVarMultisetVisitor.h"
@@ -13,6 +15,7 @@ namespace PetriEngine::Colored::Reduction {
     bool RedRulePreemptiveFiring::apply(ColoredReducer &red, const PetriEngine::PQL::ColoredUseVisitor &inQuery,
                                         QueryType queryType,
                                         bool preserveLoops, bool preserveStutter) {
+        Colored::PartitionBuilder partition(red.transitions(), red.places());
         bool continueReductions = false;
         const size_t numberofplaces = red.placeCount();
         for (uint32_t p = 0; p < numberofplaces; ++p) {
@@ -22,24 +25,30 @@ namespace PetriEngine::Colored::Reduction {
             if (place.skipped) continue;
             if (place.inhibitor) continue; //todo can do more, but eh
             if (place.marking.empty()) continue;
-            if (place._post.size() != 1)
-                continue; // could do something better, could have more transitions, but not all are enabled
-            //if (!place._pre.empty()) continue;
+
+            // - Preset can only have one transition in post
+            if (place._post.size() != 1) continue;
 
             bool ok = true;
-            if (transition_can_produce_to_place(place._post[0], p, red, std::vector<uint32_t> ())) continue;
+            // - Make sure that we do not produce tokens to something that can produce tokens to our preset. To disallow infinite use of this rule
+            if (transition_can_produce_to_place(place._post[0], p, red, std::vector<uint32_t>())) continue;
+
             const Transition &transition = red.transitions()[place._post[0]];
-            if (transition.guard) continue; //could perhaps do something
+
+            // Easiest to not handle guards, if guard, iterate through bindings
+            if (transition.guard) continue;
+
             if (transition.input_arcs.size() > 1) continue; //could also do something here?
             const auto &in = red.getInArc(p, transition);
-            if (inQuery.isPlaceUsed(in->place)) continue; // later do some more fancy where t can have more input arcs
 
+            // - Preset and postset cannot inhibit or be in query
             for (auto &out: transition.output_arcs) {
-                if (inQuery.isPlaceUsed(out.place)) {
+                if (inQuery.isPlaceUsed(out.place) || red.places()[out.place].inhibitor) {
                     ok = false;
                     break;
                 }
 
+                //next step relax this,
                 if (to_string(*out.expr) != to_string(*in->expr)) {
                     ok = false;
                     break;
@@ -48,8 +57,9 @@ namespace PetriEngine::Colored::Reduction {
 
             if (!ok) continue;
 
-            for (auto &in_arcs: transition.input_arcs) {
-                if (inQuery.isPlaceUsed(in_arcs.place)) {
+            // - Preset and postset cannot inhibit or be in query
+            for (auto &in_arc: transition.input_arcs) {
+                if (inQuery.isPlaceUsed(in_arc.place) || red.places()[in_arc.place].inhibitor) {
                     ok = false;
                     break;
                 }
@@ -63,13 +73,6 @@ namespace PetriEngine::Colored::Reduction {
             }
             place.marking *= 0;
 
-            // - Preset and postset must be disjoint (to avoid immediate infinite use)
-            // - Preset and postset cannot inhibit or be in query
-            // scale marking
-            // - Preset can only have this transition in postset
-            // - Type of places in postset is exactly that of the place in preset
-            // - Only one variable on arc
-            // Easiest to not handle guards, if guard, iterate through all bindings.
 
             _applications++;
             continueReductions = true;
@@ -78,11 +81,12 @@ namespace PetriEngine::Colored::Reduction {
         return continueReductions;
     }
 
-    bool RedRulePreemptiveFiring::transition_can_produce_to_place(unsigned int t, uint32_t p, ColoredReducer &red, std::vector<uint32_t> already_checked) const {
+    bool RedRulePreemptiveFiring::transition_can_produce_to_place(unsigned int t, uint32_t p, ColoredReducer &red,
+                                                                  std::vector<uint32_t> already_checked) const {
         const Transition &transition = red.transitions()[t];
         for (auto &out: transition.output_arcs) {
 
-            if(std::find(already_checked.begin(), already_checked.end(), out.place) != already_checked.end()) {
+            if (std::find(already_checked.begin(), already_checked.end(), out.place) != already_checked.end()) {
                 continue;
             } else {
                 already_checked.push_back(out.place);
