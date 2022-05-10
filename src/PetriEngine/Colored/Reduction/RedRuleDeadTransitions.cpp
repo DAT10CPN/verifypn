70d88ffb1e837a5036bb28726fa7dc47fc7ea35f
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-05-10 13:10:41 +0200

    woops, add back in and outsets

--- src/PetriEngine/Colored/Reduction/RedRuleDeadTransitions.cpp
+++ src/PetriEngine/Colored/Reduction/RedRuleDeadTransitions.cpp
@@ -69,6 +69,9 @@ namespace PetriEngine::Colored::Reduction {
                     }
 
                     // Only increasing loops are not ok
+                    auto inSet = PetriEngine::Colored::extractVarMultiset(*in->expr);
+                    auto outSet = PetriEngine::Colored::extractVarMultiset(*out->expr);
+
                     if ((inSet && outSet && (*inSet).isSubsetOf(*outSet)) ||
                         to_string(*out->expr) != to_string(*in->expr)) {
                         ok = false;
